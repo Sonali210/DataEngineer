2a94de5fb370a6a11862374165001a77e2ffa765
Sonali210 <sonali21031@gmail.com>
2022-07-14 12:45:42 +0000

    all data

--- dashboard/details/admin.py
+++ dashboard/details/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.Cycle6731)
+admin.site.register(models.Cycle6735)
+admin.site.register(models.Detailtemp6731)
+admin.site.register(models.Detailtemp6735)
+admin.site.register(models.Detailvol6731)
+admin.site.register(models.Detailvol6735)
+admin.site.register(models.Detail6731)
+admin.site.register(models.Detail6735)
+admin.site.register(models.Statis6731)
+admin.site.register(models.Statis6735)
--- data_file.py
+++ data_file.py
@@ -6,21 +6,43 @@ engine=create_engine("postgresql+psycopg2://nunam:nunam@165.232.183.174:5432/nun
 def sheets(data, file):
     if(data=='Cycle_67_3_5'):
         df=pd.read_excel(file,"Cycle_67_3_5")
-        df.to_sql(name='Cycle_67_3_5', con=engine, if_exists='append', index=False)
+        df.to_sql(name='details_cycle6735', con=engine, if_exists='append', index=False)
     elif(data=='DetailTemp_67_3_5'):
         df=pd.read_excel(file, "DetailTemp_67_3_5")
-        df.to_sql(name='DetailTemp_67_3_5', con=engine, if_exists='append', index=False)
+        df.to_sql(name='details_detailtemp6735', con=engine, if_exists='append', index=False)
     elif(data=='DetailVol_67_3_5'):
         df=pd.read_excel(file, "DetailVol_67_3_5")
-        df.to_sql(name='DetailVol_67_3_5', con=engine, if_exists='append', index=False)
+        df.to_sql(name='details_detailvol6735', con=engine, if_exists='append', index=False)
     elif(data=='Detail_67_3_5'):
         df=pd.read_excel(file, "Detail_67_3_5")
-        df.to_sql(name='Detail_67_3_5', con=engine, if_exists='append', index=False)
+        df.to_sql(name='details_detail6735', con=engine, if_exists='append', index=False)
     elif(data=='Statis_67_3_5'):
         df=pd.read_excel(file, "Statis_67_3_5")
-        df.to_sql(name='Statis_67_3_5', con=engine, if_exists='append', index=False)
+        df.to_sql(name='details_statis6735', con=engine, if_exists='append', index=False)
 
-
+def sheets1(data, file):
+    if(data=='Cycle_67_3_1'):
+        df=pd.read_excel(file,"Cycle_67_3_1")
+        df.to_sql(name='details_cycle6731', con=engine, if_exists='append', index=False)
+    elif(data=='DetailTemp_67_3_1'):
+        df=pd.read_excel(file, "DetailTemp_67_3_1")
+        df.to_sql(name='details_detailtemp6731', con=engine, if_exists='append', index=False)
+    elif(data=='DetailVol_67_3_1'):
+        df=pd.read_excel(file, "DetailVol_67_3_1")
+        df.to_sql(name='details_detailvol6731', con=engine, if_exists='append', index=False)
+    elif(data=='Detail_67_3_1'):
+        df=pd.read_excel(file, "Detail_67_3_1")
+        df.to_sql(name='details_detail6731', con=engine, if_exists='append', index=False)
+    elif(data=='Statis_67_3_1'):
+        df=pd.read_excel(file, "Statis_67_3_1")
+        df.to_sql(name='details_statis6731', con=engine, if_exists='append', index=False)
+        
+with pd.ExcelFile('5329.xls') as xls:
+    for sheet_name in xls.sheet_names:
+        print("name:=======>", sheet_name)
+        sheets1(sheet_name, xls)
+        
+        
 with pd.ExcelFile('5308.xls') as xls:
     for sheet_name in xls.sheet_names:
         print("name:=======>", sheet_name)
